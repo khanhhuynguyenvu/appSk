5a43826a35f6da39087c7757387cfba086f94f38
khanhhuynguyenvu <khanhhuynguyenvu@gmail.com>
2020-04-01 22:45:45 +0700

    lan 3

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ from sklearn import datasets, svm
 app = Flask(__name__)
 
 # Load Dataset from scikit-learn.
-digits = datasets.load_digits()
+# digits = datasets.load_digits()
 
-@app.route('/')
-def hello():
-    clf = svm.SVC(gamma=0.001, C=100.)
-    clf.fit(digits.data[:-1], digits.target[:-1])
-    prediction = clf.predict(digits.data[-1:])
+# @app.route('/')
+# def hello():
+#     clf = svm.SVC(gamma=0.001, C=100.)
+#     clf.fit(digits.data[:-1], digits.target[:-1])
+#     prediction = clf.predict(digits.data[-1:])
 
-    return jsonify({'prediction': repr(prediction)})
+#     return jsonify({'prediction': repr(prediction)})
 @app.route('/hi')
 def hi():
 	return "hello may cung"
